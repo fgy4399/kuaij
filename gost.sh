594ff9f12202e09706c49e15afd6e2af4f31800d
fgy4399 <73323116+fgy4399@users.noreply.github.com>
2023-02-24 20:17:29 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -111,7 +111,7 @@ function Install_ct() {
     mv gost-linux-"$bit"-"$ct_new_ver" gost
     mv gost /usr/bin/gost
     chmod -R 777 /usr/bin/gost
-    wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
+    wget --no-check-certificate https://raw.githubusercontent.com/fgy4399/kuaij/main/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
     mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
   fi
 
